ec248c6f7fcf0ff6bb5067312f950a5e9e1ca34a
Christopher Erin <chris.erin@gmail.com>
2013-11-26 18:34:58 -0600

    Coffeescript functionality for with --coffee.
    * repl command only

--- lib/req/commands/repl.rb
+++ lib/req/commands/repl.rb
@@ -7,11 +7,13 @@ module Req
         context.desc 'repl', 'open js repl in the context of your webpage'
         context.option :empty_page, :type => :boolean
         context.option :jquery, :type => :boolean
+        context.option :coffee, :type => :boolean
         context.option :exec
       end
 
       def self.run
         phantom_options = options.keys.join(",")
+        puts phantom_options
         js_string       = String.new if options.exec == "exec"
         Req::EmptyPageDir.create if options.empty_page? || options.jquery? || Req::Dir.latest.nil?
 
